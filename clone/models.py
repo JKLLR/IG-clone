a6e8aa0e6c98b23dc0deabaee0ea6963324e1e1c
JKLLR <jeffhuria@gmail.com>
2022-06-06 14:27:56 +0300

    connect to postgres and make initial migrations

--- clone/models.py
+++ clone/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    name = models.CharField(max_length = 30)
+    bio = models.CharField(max_length=300)
+    profile_photo = models.ImageField( default='profile/default.png')
+
+    def __str__(self):
+        return self.name
+
+
+
--- instagram/settings.py
+++ instagram/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&4r9i*1ytf6^&z0^jf5fgvp*yl2(2@rz8xtb!6lyyos)b(&uod'
+# SECRET_KEY = 'django-insecure-&4r9i*1ytf6^&z0^jf5fgvp*yl2(2@rz8xtb!6lyyos)b(&uod'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,9 +79,11 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD')
     }
 }
 
@@ -108,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
