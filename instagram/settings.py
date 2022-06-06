60fc9a7a48d0b7a89822b55f3fc31bd1975ff4cd
JKLLR <jeffhuria@gmail.com>
2022-06-06 05:24:22 +0300

    create home template

--- instagram/settings.py
+++ instagram/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'clone.apps.CloneConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
