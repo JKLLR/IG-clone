0651107decb9d7647a3db1ed73117de5ac87f81c
JKLLR <jeffhuria@gmail.com>
2022-06-07 22:16:05 +0300

    run test post model

--- clone/tests.py
+++ clone/tests.py
@@ -13,3 +13,18 @@ class TestUserProfile(TestCase):
 
     def test_instance(self):
         self.assertTrue(isinstance(self.profile, UserProfile))
+
+
+class TestPost(TestCase):
+    def setUp(self):
+        self.user = User(first_name="jeff", last_name="huria",
+                         username="jeffhuria", email="jeffhuria21@gmail.com",)
+        self.user.save()
+        self.profile = UserProfile(user=self.user, bio="my bio",)
+        self.profile.save()
+        self.test_post = Post(name="Post", caption="test caption", 
+                              profile = self.user, user_profile=self.profile)
+        self.test_post.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.test_post, Post))
