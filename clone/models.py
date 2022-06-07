293439321d7f20650c086c1e325ba8797c96f248
JKLLR <jeffhuria@gmail.com>
2022-06-07 12:40:10 +0300

    update the profile model

--- clone/models.py
+++ clone/models.py
@@ -2,14 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+profile_photo = models.ImageField( default='profile/default.png')
 
-class Profile(models.Model):
-    name = models.CharField(max_length = 30)
-    bio = models.CharField(max_length=300)
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_photo = models.ImageField( default='profile/default.png')
+    bio = models.TextField(blank=True)
+    followers = models.ManyToManyField(User, related_name="followers", blank=True)
+    following = models.ManyToManyField(User, related_name="following", blank=True)
 
     def __str__(self):
-        return self.name
+        return self.bio
 
+    @classmethod
+    def search_by_user(cls,search_term):
+        instauser = cls.objects.filter(user__icontains=search_term)
+        return instauser
 
 
