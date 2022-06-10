a7b69044f6829086a47a733d8cf4efe109ee5acf
JKLLR <jeffhuria@gmail.com>
2022-06-10 17:28:40 +0300

    heroku deploy

--- instagram/settings.py
+++ instagram/settings.py
@@ -31,7 +31,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
 
 # ALLOWED_HOSTS = []
 
