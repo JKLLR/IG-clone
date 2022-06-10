b737ef8d181b121288035b1370f3c7ee247ed624
JKLLR <jeffhuria@gmail.com>
2022-06-10 19:28:26 +0300

    heroku deploy

--- clone/urls.py
+++ clone/urls.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
 
 
 urlpatterns=[
@@ -15,6 +17,6 @@ urlpatterns=[
     path('profile/', views.profile, name='profile'),
     path('like/(<image_id>\d+)',views.like, name = 'postlike'),
 
-
-  
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
